b71582f6aac198e31b30eb6544049b135053127c
bmangez <Benoit.Mangez@mcl-technologies.com>
2018-07-02 11:19:36 +0200

    Compliance with http specifications.(https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.2)  401 : The response MUST include a WWW-Authenticate header field containing a challenge applicable to the requested resource.  403 : The server understood the request, but is refusing to fulfill it.

--- src/main/java/com/iota/iri/service/API.java
+++ src/main/java/com/iota/iri/service/API.java
@@ -1002,7 +1002,7 @@ public class API {
         if (res instanceof ErrorResponse) {
             exchange.setStatusCode(400); // bad request
         } else if (res instanceof AccessLimitedResponse) {
-            exchange.setStatusCode(401); // api method not allowed
+            exchange.setStatusCode(403); // api method not allowed
         } else if (res instanceof ExceptionResponse) {
             exchange.setStatusCode(500); // internal error
         }
